1fe9a7a02c200a6b13ffb17ecb16814e2a552d3c
P Campbell <pcampbell.edu@gmail.com>
2018-12-09 23:09:49 -0500

    convert to kotlin

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# week04-localisation
+# topic05-localisation
 # Localisation 
 Sample code using android localisation
+Default English 
+Other French, German, Spanish, French Canada, Italian, Russian
+
 @string/app_name etc
+
 See https://www.w3.org/International/questions/qa-i18n
 
---
+++ app/src/main/java/cs518/sample/localisation/MainActivity.kt
@@ -0,0 +1,14 @@
+package cs518.sample.localisation
+
+import android.os.Bundle
+import android.app.Activity
+import android.view.Menu
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
